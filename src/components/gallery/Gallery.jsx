6e3d830eedf59eca2a70b84d5600908d19493ebf
EsotericZ <cjsand03@gmail.com>
2023-06-03 13:16:21 -0600

    more pics

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -11,6 +11,25 @@ import pic10 from '../../images/pic10.jpg';
 import pic11 from '../../images/pic11.jpg';
 import pic12 from '../../images/pic12.jpg';
 import pic13 from '../../images/pic13.jpg';
+import pic14 from '../../images/pic14.jpg';
+import pic15 from '../../images/pic15.jpg';
+import pic16 from '../../images/pic16.jpg';
+import pic17 from '../../images/pic17.jpg';
+import pic18 from '../../images/pic18.jpg';
+import pic19 from '../../images/pic19.jpg';
+import pic20 from '../../images/pic20.jpg';
+import pic21 from '../../images/pic21.jpg';
+import pic22 from '../../images/pic22.jpg';
+import pic23 from '../../images/pic23.jpg';
+import pic24 from '../../images/pic24.jpg';
+import pic25 from '../../images/pic25.jpg';
+import pic26 from '../../images/pic26.jpg';
+import pic27 from '../../images/pic27.jpg';
+import pic28 from '../../images/pic28.jpg';
+import pic29 from '../../images/pic29.jpg';
+import pic30 from '../../images/pic30.jpg';
+import pic31 from '../../images/pic31.jpg';
+
 
 import {
     MDBCarousel,
@@ -104,6 +123,114 @@ export const Gallery = () => {
                         className='w-100'
                         itemId={13}
                         src={pic13}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={14}
+                        src={pic14}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={15}
+                        src={pic15}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={16}
+                        src={pic16}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={17}
+                        src={pic17}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={18}
+                        src={pic18}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={19}
+                        src={pic19}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={20}
+                        src={pic20}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={21}
+                        src={pic21}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={22}
+                        src={pic22}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={23}
+                        src={pic23}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={24}
+                        src={pic24}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={25}
+                        src={pic25}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={26}
+                        src={pic26}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={27}
+                        src={pic27}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={28}
+                        src={pic28}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={29}
+                        src={pic29}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={30}
+                        src={pic30}
+                        alt='...'
+                    />
+                    <MDBCarouselItem
+                        className='w-100'
+                        itemId={31}
+                        src={pic31}
                         alt='...'
                     />
                 </div>
